3c200930690687b9afd40b4e28d71549c6165161
punitha-stp <74604058+punitha-stp@users.noreply.github.com>
2020-11-19 14:53:40 +0530

    Module 3

---
+++ forLoopDemoLWC.js
@@ -0,0 +1,9 @@
+import Car1 from '@salesforce/resourceUrl/Car1';
+import { LightningElement } from 'lwc';
+
+export default class ForLoopDemoLWC extends LightningElement {
+    tableContent = [{Index:'I', Id:'C1', Name:'Amy Taylor', Email:'amy.Taylor@gm.com' }, {Index:'1', Id:'C2', Name:'John Williams', Email:'john.wil@gm.com' }, 
+                    {Index:'2', Id:'C3', Name:'Jack Hill', Email:'jack.hill@gm.com' }, {Index:'3', Id:'C4', Name:'Amulya Behur', Email:'amul.behur@gm.com'}];
+
+    
+}
